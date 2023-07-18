f6b2815066a549a1ed1351e88fbc22b1bfa033e4
Friedrich Horschig <friedrich.horschig@mailbox.org>
2023-07-18 16:56:49 +0200

    feat: log uploaded text

--- src/handlers/add-work.go
+++ src/handlers/add-work.go
@@ -1,9 +1,11 @@
 package handlers
 
 import (
+	"io/ioutil"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/rs/zerolog/log"
 )
 
 type AddWorkHandler interface {
@@ -19,5 +21,11 @@ func NewAddWorkHandler() AddWorkHandler {
 }
 
 func (handler *AddWorkHandlerImpl) PostWork(ctx echo.Context) error {
+	body, err := ioutil.ReadAll(ctx.Request().Body)
+	if err != nil {
+		log.Error().Err(err).Msg("Error reading request body")
+		return ctx.JSON(http.StatusBadRequest, "Error reading request body")
+	}
+	log.Info().Msg(string(body))
 	return ctx.JSON(http.StatusOK, "Hello World")
 }
