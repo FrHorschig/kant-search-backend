013427c8191e2a8c5b13a7012fc46616ed318a18
Friedrich Horschig <friedrich.horschig@mailbox.org>
2023-09-17 12:11:49 +0200

    build: adjust database environment variables

--- deployment/local_env.bash
+++ deployment/local_env.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 export ALLOW_ORIGINS=https://localhost:4200
-export DB_USER=postgres
-export DB_PASSWORD=postgres
-export DB_NAME=postgres
+
 export DB_HOST=localhost
 export DB_PORT=5432
+export DB_USER=kantsearch
+export DB_PASSWORD=kantsearch
+export DB_NAME=kantsearch
+export DB_SSLMODE=disable
--- src/util/db.go
+++ src/util/db.go
@@ -8,12 +8,12 @@ import (
 )
 
 func InitDbConnection() *sql.DB {
-	connStr := "user=" + os.Getenv("DB_USER") +
+	connStr := "host=" + os.Getenv("DB_HOST") +
+		" port=" + os.Getenv("DB_PORT") +
+		" user=" + os.Getenv("DB_USER") +
 		" password=" + os.Getenv("DB_PASSWORD") +
 		" dbname=" + os.Getenv("DB_NAME") +
-		" host=" + os.Getenv("DB_HOST") +
-		" port=" + os.Getenv("DB_PORT") +
-		" sslmode=disable"
+		" sslmode=" + os.Getenv("DB_SSLMODE")
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
