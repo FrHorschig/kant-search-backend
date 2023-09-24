bb6dbc03c2cc70fb6d309746fa3e4a6921328db8
Friedrich Horschig <friedrich.horschig@mailbox.org>
2023-09-24 15:55:30 +0200

    feat: combine search, excluded and optional terms

--- src/database/repository/search_repo.go
+++ src/database/repository/search_repo.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"strings"
 
 	"github.com/FrHorschig/kant-search-backend/database/model"
 	"github.com/lib/pq"
@@ -28,22 +29,18 @@ func NewSearchRepo(db *sql.DB) SearchRepo {
 }
 
 func (repo *searchRepoImpl) SearchParagraphs(ctx context.Context, criteria model.SearchCriteria) ([]model.SearchResult, error) {
-	snippetParams := `FragmentDelimiter="...<br>... ",
-		MaxFragments=10,
-		MaxWords=16,
-		MinWords=6`
-	textParams := `MaxWords=99999, MinWords=99998`
+	snippetParams, textParams := buildParams()
 	query := `SELECT
 			p.id, 
-			ts_headline('german', p.content, plainto_tsquery('german', $2), $3),
-			ts_headline('german', p.content, plainto_tsquery('german', $2), $4),
+			ts_headline('german', p.content, to_tsquery('german', $2), $3),
+			ts_headline('german', p.content, to_tsquery('german', $2), $4),
 			p.pages,
 			p.work_id
-		FROM paragraphs p
+		FROM paragraph p
 		WHERE work_id = ANY($1) AND search @@ plainto_tsquery('german', $2)
 		ORDER BY p.work_id, p.id`
 
-	rows, err := repo.db.QueryContext(ctx, query, pq.Array(criteria.WorkIds), criteria.SearchTerms[0], snippetParams, textParams)
+	rows, err := repo.db.QueryContext(ctx, query, pq.Array(criteria.WorkIds), buildTerms(criteria), snippetParams, textParams)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return []model.SearchResult{}, nil
@@ -61,6 +58,29 @@ func (repo *searchRepoImpl) SearchParagraphs(ctx context.Context, criteria model
 func (repo *searchRepoImpl) SearchSentences(ctx context.Context, criteria model.SearchCriteria) ([]model.SearchResult, error) {
 	// TODO implement me
 	return []model.SearchResult{}, nil
+}
+
+func buildParams() (snippetParams string, textParams string) {
+	snippetParams = `FragmentDelimiter="...<br>... ",
+		MaxFragments=10,
+		MaxWords=16,
+		MinWords=6`
+	textParams = `MaxWords=99999, MinWords=99998`
+	return
+}
+
+func buildTerms(c model.SearchCriteria) string {
+	var builder strings.Builder
+	builder.WriteString(strings.Join(c.SearchTerms, " & "))
+	if len(c.ExcludedTerms) > 0 {
+		builder.WriteString(" & !")
+		builder.WriteString(strings.Join(c.ExcludedTerms, " & !"))
+	}
+	if len(c.OptionalTerms) > 0 {
+		builder.WriteString(" | ")
+		builder.WriteString(strings.Join(c.OptionalTerms, " | "))
+	}
+	return builder.String()
 }
 
 func scanSearchMatchRow(rows *sql.Rows) ([]model.SearchResult, error) {
