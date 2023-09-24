fc657d0f18fc6675826d2dfb166d2b4622bbe5bb
Friedrich Horschig <friedrich.horschig@mailbox.org>
2023-09-24 16:39:57 +0200

    test: add unit tests for sentence search

--- src/database/repository/search_repo_unit_test.go
+++ src/database/repository/search_repo_unit_test.go
@@ -87,3 +87,76 @@ func TestSearchParagraphsWrongRows(t *testing.T) {
 	assert.NotNil(t, err)
 	assert.Empty(t, matches)
 }
+
+func TestSearchSentencesDatabaseError(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	repo := &searchRepoImpl{db: db}
+	dbErr := fmt.Errorf("database error")
+
+	criteria := model.SearchCriteria{
+		WorkIds:     []int32{1},
+		SearchTerms: []string{"Maxime"},
+	}
+
+	// GIVEN
+	mock.ExpectQuery(anyQuery).WillReturnError(dbErr)
+
+	// WHEN
+	matches, err := repo.SearchSentences(context.Background(), criteria)
+
+	// THEN
+	assert.Equal(t, dbErr, err)
+	assert.Empty(t, matches)
+}
+
+func TestSearchSentencesNoRows(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	repo := &searchRepoImpl{db: db}
+
+	criteria := model.SearchCriteria{
+		WorkIds:     []int32{1},
+		SearchTerms: []string{"Maxime"},
+	}
+
+	// GIVEN
+	mock.ExpectQuery(anyQuery).WillReturnError(sql.ErrNoRows)
+
+	// WHEN
+	matches, err := repo.SearchSentences(context.Background(), criteria)
+
+	// THEN
+	assert.Nil(t, err)
+	assert.Empty(t, matches)
+}
+
+func TestSearchSentencesWrongRows(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	repo := &searchRepoImpl{db: db}
+
+	criteria := model.SearchCriteria{
+		WorkIds:     []int32{1},
+		SearchTerms: []string{"Maxime"},
+	}
+
+	// GIVEN
+	mock.ExpectQuery(anyQuery).WillReturnRows(sqlmock.NewRows([]string{"abc", "def"}).AddRow(1, 1))
+
+	// WHEN
+	matches, err := repo.SearchSentences(context.Background(), criteria)
+
+	// THEN
+	assert.NotNil(t, err)
+	assert.Empty(t, matches)
+}
