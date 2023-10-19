ad42670e59aa028e312b273c673ec80568a93e4b
Friedrich Horschig <friedrich.horschig@mailbox.org>
2023-10-19 18:51:07 +0200

    feat: read new fields from database

--- src/database/paragraph_repo.go
+++ src/database/paragraph_repo.go
@@ -30,8 +30,8 @@ func NewParagraphRepo(db *sql.DB) ParagraphRepo {
 
 func (repo *paragraphRepoImpl) Insert(ctx context.Context, paragraph model.Paragraph) (int32, error) {
 	var id int32
-	query := `INSERT INTO paragraphs (content, pages, work_id) VALUES ($1, $2, $3) RETURNING id`
-	err := repo.db.QueryRowContext(ctx, query, paragraph.Text, pq.Array(paragraph.Pages), paragraph.WorkId).Scan(&id)
+	query := `INSERT INTO paragraphs (content, pages, work_id, heading_level, footnote_name) VALUES ($1, $2, $3, $4, $5) RETURNING id`
+	err := repo.db.QueryRowContext(ctx, query, paragraph.Text, pq.Array(paragraph.Pages), paragraph.WorkId, paragraph.HeadingLevel, paragraph.FootnoteName).Scan(&id)
 	if err != nil {
 		return 0, err
 	}
@@ -40,7 +40,7 @@ func (repo *paragraphRepoImpl) Insert(ctx context.Context, paragraph model.Parag
 }
 
 func (repo *paragraphRepoImpl) SelectAll(ctx context.Context, workId int32) ([]model.Paragraph, error) {
-	query := `SELECT id, content, pages, work_id FROM paragraphs WHERE work_id = $1 ORDER BY id`
+	query := `SELECT id, content, pages, work_id, heading_level, footnote_name FROM paragraphs WHERE work_id = $1 ORDER BY id`
 	rows, err := repo.db.QueryContext(ctx, query, workId)
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -64,7 +64,9 @@ func (repo *paragraphRepoImpl) Search(ctx context.Context, criteria model.Search
 			ts_headline('german', p.content, to_tsquery('german', $2), $4),
 			p.pages,
 			p.id, 
-			p.work_id
+			p.work_id,
+			p.heading_level,
+			p.footnote_name
 		FROM paragraphs p
 		WHERE p.work_id = ANY($1) AND p.search @@ to_tsquery('german', $2)
 		ORDER BY p.work_id, p.id`
@@ -92,17 +94,17 @@ func (repo *paragraphRepoImpl) DeleteByWorkId(ctx context.Context, workId int32)
 func scanParagraphRows(rows *sql.Rows) ([]model.Paragraph, error) {
 	paragraphs := make([]model.Paragraph, 0)
 	for rows.Next() {
-		var work model.Paragraph
+		var paragraph model.Paragraph
 		var pages pq.Int64Array
-		err := rows.Scan(&work.Id, &work.Text, &pages, &work.WorkId)
+		err := rows.Scan(&paragraph.Id, &paragraph.Text, &pages, &paragraph.WorkId, &paragraph.HeadingLevel, &paragraph.FootnoteName)
 		if err != nil {
 			return nil, fmt.Errorf("paragraph row scan failed: %v", err)
 		}
-		work.Pages = make([]int32, len(pages))
+		paragraph.Pages = make([]int32, len(pages))
 		for i, page := range pages {
-			work.Pages[i] = int32(page)
+			paragraph.Pages[i] = int32(page)
 		}
-		paragraphs = append(paragraphs, work)
+		paragraphs = append(paragraphs, paragraph)
 	}
 	return paragraphs, nil
 }
