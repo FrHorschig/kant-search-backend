50bd9082bf9e0f3a61fc1a7ba38862058c4054a8
Friedrich Horschig <friedrich.horschig@mailbox.org>
2023-10-20 15:38:17 +0200

    fix: fix paragraph merge errors

--- src/core/upload/internal/transform/merge.go
+++ src/core/upload/internal/transform/merge.go
@@ -11,19 +11,18 @@ type metadata struct {
 	isPageEndPara   bool
 	isPageStartPara bool
 	isIncomplete    bool
-	FnRefs          []string
+	fnRefs          []string
 }
 
 func MergeParagraphs(pars []model.Paragraph) []model.Paragraph {
 	md, fnByName := mapToMetadata(pars)
 	for i := len(md) - 1; i >= 1; i-- {
-		curr := md[i]
-		prev := md[i-1]
-		if curr.isPageStartPara && prev.isPageEndPara && prev.isIncomplete {
-			prev.paragraph.Text += curr.paragraph.Text
-			prev.paragraph.Pages = append(prev.paragraph.Pages, curr.paragraph.Pages...)
-			prev.FnRefs = append(prev.FnRefs, curr.FnRefs...)
-			curr.paragraph = nil
+		h := i - 1
+		if md[i].isPageStartPara && md[h].isPageEndPara && md[h].isIncomplete {
+			md[h].paragraph.Text += " " + md[i].paragraph.Text
+			md[h].paragraph.Pages = append(md[h].paragraph.Pages, md[i].paragraph.Pages...)
+			md[h].fnRefs = append(md[h].fnRefs, md[i].fnRefs...)
+			md[i].paragraph = nil
 		}
 	}
 
@@ -33,7 +32,7 @@ func MergeParagraphs(pars []model.Paragraph) []model.Paragraph {
 			continue
 		}
 		merged = append(merged, *m.paragraph)
-		for _, fnRef := range m.FnRefs {
+		for _, fnRef := range m.fnRefs {
 			merged = append(merged, *fnByName[fnRef])
 		}
 	}
@@ -45,22 +44,22 @@ func mapToMetadata(pars []model.Paragraph) ([]metadata, map[string]*model.Paragr
 	fnByName := make(map[string]*model.Paragraph)
 	for i, p := range pars {
 		if p.FootnoteName != nil {
-			fnByName[*p.FootnoteName] = &p
+			fnByName[*p.FootnoteName] = &pars[i]
 			continue
 		}
 		if p.HeadingLevel != nil {
-			md = append(md, metadata{paragraph: &p})
+			md = append(md, metadata{paragraph: &pars[i]})
 			continue
 		}
 		m := metadata{
-			paragraph:    &p,
+			paragraph:    &pars[i],
 			isIncomplete: !isEndPunctuation(p.Text[len(p.Text)-1]),
-			FnRefs:       findFnRefs(p.Text),
+			fnRefs:       findFnRefs(p.Text),
 		}
-		if i > 0 && p.Pages[0] != pars[i-1].Pages[0] {
+		if i > 0 && p.Pages[0] != pars[i-1].Pages[0] || i == 0 {
 			m.isPageStartPara = true
 		}
-		if i < len(pars)-1 && p.Pages[0] != pars[i+1].Pages[0] {
+		if i < len(pars)-1 && p.Pages[0] != pars[i+1].Pages[0] || i == len(pars)-1 {
 			m.isPageEndPara = true
 		}
 		md = append(md, m)
@@ -73,6 +72,10 @@ func isEndPunctuation(b byte) bool {
 }
 
 func findFnRefs(text string) []string {
-	matches := regexp.MustCompile(`\{fn\d+\.\d+\}`).FindAllString(text, -1)
-	return matches
+	matches := regexp.MustCompile(`\{fn(\d+\.\d+)\}`).FindAllStringSubmatch(text, -1)
+	fnRefs := make([]string, 0)
+	for _, m := range matches {
+		fnRefs = append(fnRefs, m[1])
+	}
+	return fnRefs
 }
