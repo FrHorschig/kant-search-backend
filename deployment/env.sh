19ff17886ff2279a605e2e1d2fac14ac1322f0bf
Friedrich Horschig <friedrich.horschig@mailbox.org>
2024-06-19 16:33:35 +0200

    build: add script to set env vars to release

---
+++ deployment/env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export KSDB_USER="kantsearch"
+export KSDB_PASSWORD="kantsearch"
+export KSDB_NAME="kantsearch"
+export KSDB_PORT="5432"
+export KSGO_DB_HOST="localhost"
+export KSGO_DB_SSLMODE="false"
+export KSGO_ALLOW_ORIGINS="localhost"
+export KSGO_DISABLE_SSL="false"
+export KSGO_CERT_PATH="/etc/ssl/ksgo/ksgo.crt"
+export KSGO_KEY_PATH="/etc/ssl/ksgo/ksgo.key"
+export KSGO_PYTHON_BIN_PATH="/home/kantsearch/venv/bin/python3"
+export KSGO_PYTHON_SCRIPT_PATH="/home/kantsearch/split_text.py"
