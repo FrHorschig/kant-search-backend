a7fff8993feeffc8e0318776629fc4a75eb3c016
Friedrich Horschig <friedrich.horschig@mailbox.org>
2025-01-02 18:24:13 +0100

    extract common code

--- src/api/upload/upload.go
+++ src/api/upload/upload.go
@@ -39,92 +39,54 @@ func (rec *uploadHandlerImpl) PostVolume(ctx echo.Context) error {
 		log.Error().Err(err).Msg(msg)
 		return errors.BadRequest(ctx, msg)
 	}
-
 	body, err := io.ReadAll(ctx.Request().Body)
 	if err != nil {
 		msg := fmt.Sprintf("Error reading request body: %v", err.Error())
 		log.Error().Err(err).Msg(msg)
 		return errors.BadRequest(ctx, msg)
 	}
+
 	switch {
 	case volNum >= 1 && volNum <= 9:
-		var vol abt1.Band
-		err = xml.Unmarshal(body, &vol)
-		if err != nil {
-			msg := fmt.Sprintf("Error reading request body: %v", err.Error())
-			log.Error().Err(err).Msg(msg)
-			return errors.BadRequest(ctx, msg)
-		}
-		err = rec.volumeProcessor.ProcessAbt1(ctx.Request().Context(), int32(volNum), vol)
-		if err != nil {
-			msg := fmt.Sprintf("Error processing xml data: %v", err.Error())
-			log.Error().Err(err).Msg(msg)
-			return errors.BadRequest(ctx, msg)
-		}
-
+		return rec.processVolume(ctx, volNum, body, func(vol interface{}) error {
+			return rec.volumeProcessor.ProcessAbt1(ctx.Request().Context(), int32(volNum), vol.(abt1.Band))
+		})
 	case volNum >= 10 && volNum <= 13:
-		var vol abt2.Band
-		err = xml.Unmarshal(body, &vol)
-		if err != nil {
-			msg := fmt.Sprintf("Error reading request body: %v", err.Error())
-			log.Error().Err(err).Msg(msg)
-			return errors.BadRequest(ctx, msg)
-		}
-		err = rec.volumeProcessor.ProcessAbt2(ctx.Request().Context(), int32(volNum), vol)
-		if err != nil {
-			msg := fmt.Sprintf("Error processing xml data: %v", err.Error())
-			log.Error().Err(err).Msg(msg)
-			return errors.BadRequest(ctx, msg)
-		}
-
+		return rec.processVolume(ctx, volNum, body, func(vol interface{}) error {
+			return rec.volumeProcessor.ProcessAbt2(ctx.Request().Context(), int32(volNum), vol.(abt2.Band))
+		})
 	case volNum == 14:
-		var vol vol14.Band
-		err = xml.Unmarshal(body, &vol)
-		if err != nil {
-			msg := fmt.Sprintf("Error reading request body: %v", err.Error())
-			log.Error().Err(err).Msg(msg)
-			return errors.BadRequest(ctx, msg)
-		}
-		err = rec.volumeProcessor.ProcessVol14(ctx.Request().Context(), int32(volNum), vol)
-		if err != nil {
-			msg := fmt.Sprintf("Error processing xml data: %v", err.Error())
-			log.Error().Err(err).Msg(msg)
-			return errors.BadRequest(ctx, msg)
-		}
+		return rec.processVolume(ctx, volNum, body, func(vol interface{}) error {
+			return rec.volumeProcessor.ProcessVol14(ctx.Request().Context(), int32(volNum), vol.(vol14.Band))
+		})
 
 	case volNum >= 15 && volNum <= 19:
-		var vol abt31.Band
-		err = xml.Unmarshal(body, &vol)
-		if err != nil {
-			msg := fmt.Sprintf("Error reading request body: %v", err.Error())
-			log.Error().Err(err).Msg(msg)
-			return errors.BadRequest(ctx, msg)
-		}
-		err = rec.volumeProcessor.ProcessAbt31(ctx.Request().Context(), int32(volNum), vol)
-		if err != nil {
-			msg := fmt.Sprintf("Error processing xml data: %v", err.Error())
-			log.Error().Err(err).Msg(msg)
-			return errors.BadRequest(ctx, msg)
-		}
-
+		return rec.processVolume(ctx, volNum, body, func(vol interface{}) error {
+			return rec.volumeProcessor.ProcessAbt31(ctx.Request().Context(), int32(volNum), vol.(abt31.Band))
+		})
 	case volNum >= 20 && volNum <= 23:
-		var vol abt32.Band
-		err = xml.Unmarshal(body, &vol)
-		if err != nil {
-			msg := fmt.Sprintf("Error reading request body: %v", err.Error())
-			log.Error().Err(err).Msg(msg)
-			return errors.BadRequest(ctx, msg)
-		}
-		err = rec.volumeProcessor.ProcessAbt32(ctx.Request().Context(), int32(volNum), vol)
-		if err != nil {
-			msg := fmt.Sprintf("Error processing xml data: %v", err.Error())
-			log.Error().Err(err).Msg(msg)
-			return errors.BadRequest(ctx, msg)
-		}
-
+		return rec.processVolume(ctx, volNum, body, func(vol interface{}) error {
+			return rec.volumeProcessor.ProcessAbt32(ctx.Request().Context(), int32(volNum), vol.(abt32.Band))
+		})
 	default:
 		msg := "The volume number must be a number from 1 to 23"
 		log.Error().Msg(msg)
+		return errors.BadRequest(ctx, msg)
+	}
+}
+
+func (rec *uploadHandlerImpl) processVolume(ctx echo.Context, volNum int64, body []byte, processFunc func(interface{}) error) error {
+	var vol interface{}
+	err := xml.Unmarshal(body, &vol)
+	if err != nil {
+		msg := fmt.Sprintf("Error unmarshaling request body: %v", err.Error())
+		log.Error().Err(err).Msg(msg)
+		return errors.BadRequest(ctx, msg)
+	}
+
+	if err := processFunc(vol); err != nil {
+		msg := fmt.Sprintf("Error processing XML data for volume %d: %v", volNum, err.Error())
+		log.Error().Err(err).Msg(msg)
 		return errors.BadRequest(ctx, msg)
 	}
 
