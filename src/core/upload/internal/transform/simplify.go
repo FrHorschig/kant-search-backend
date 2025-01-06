0381cd9d72ea09eca9040e45d56183e5692b5dcd
Friedrich Horschig <friedrich.horschig@mailbox.org>
2025-01-06 20:03:31 +0100

    remove <seite> replacement

--- src/core/upload/internal/transform/simplify.go
+++ src/core/upload/internal/transform/simplify.go
@@ -5,10 +5,9 @@ import (
 )
 
 func Simplify(xml string) string {
+	// We don't replace <seite ...> with {p...}, because the <seite> elements are sometimes on the same level as the headings and paragraphs. Replacing them here would make finding them later more difficult, so we do the <seite> replacement manually at another place.
 	reZeile := regexp.MustCompile(`<zeile\s+nr="(\d+)"\s*/>`)
 	xml = reZeile.ReplaceAllString(xml, `{l$1}`)
-	reSeite := regexp.MustCompile(`<seite\s*[^>]\s*nr="(\d+)"\s*[^>]*\s*/>`)
-	xml = reSeite.ReplaceAllString(xml, `{p$1}`)
 	reTrenn := regexp.MustCompile(`<trenn\s*/>`)
 	xml = reTrenn.ReplaceAllString(xml, "")
 	return xml
