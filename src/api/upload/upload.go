39be4f81f525c5299b52b894cf07aef6cc98f689
Friedrich Horschig <friedrich.horschig@mailbox.org>
2025-01-06 17:31:38 +0100

    remove volume number from POST path

--- src/api/upload/upload.go
+++ src/api/upload/upload.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/beevik/etree"
 	"github.com/frhorschig/kant-search-backend/api/upload/errors"
@@ -28,14 +29,39 @@ func NewUploadHandler(volumeProcessor upload.UploadProcessor) UploadHandler {
 }
 
 func (rec *uploadHandlerImpl) PostVolume(ctx echo.Context) error {
-	volNum, err := strconv.ParseInt(ctx.Param("volumeNumber"), 10, 32)
+	body, err := io.ReadAll(ctx.Request().Body)
 	if err != nil {
-		msg := fmt.Sprintf("error parsing volume number: %v", err.Error())
-		log.Error().Err(err).Msg(msg)
+		msg := fmt.Sprintf("error reading request body: %v", err.Error())
+		log.Error().Msg(msg)
+		return errors.JsonError(ctx, http.StatusBadRequest, msg)
+	}
+	doc := etree.NewDocument()
+	if err := doc.ReadFromBytes(body); err != nil {
+		msg := fmt.Sprintf("error unmarshaling request body: %v", err.Error())
+		log.Error().Msg(msg)
+		return errors.JsonError(ctx, http.StatusBadRequest, msg)
+	}
+
+	band := doc.FindElement("//band")
+	if band == nil || band.SelectAttr("nr") == nil {
+		msg := "missing element 'band' with attribute 'nr'"
+		log.Error().Msg(msg)
+		return errors.JsonError(ctx, http.StatusBadRequest, msg)
+	}
+	nrStr := strings.TrimLeft(band.SelectAttr("nr").Value, "0")
+	if nrStr == "" {
+		msg := "the volume number is 0, but it must be between 1 and 9"
+		log.Error().Msg(msg)
+		return errors.JsonError(ctx, http.StatusBadRequest, msg)
+	}
+	volNum, err := strconv.Atoi(nrStr)
+	if err != nil {
+		msg := fmt.Sprintf("attribute 'nr' of element 'band' can't be converted to a number: %v", err.Error())
+		log.Error().Msg(msg)
 		return errors.JsonError(ctx, http.StatusBadRequest, msg)
 	}
 	if volNum < 1 {
-		msg := "the volume number must be between 1 and 9"
+		msg := fmt.Sprintf("the volume number is %d, but it must be between 1 and 9", volNum)
 		log.Error().Msg(msg)
 		return errors.JsonError(ctx, http.StatusBadRequest, msg)
 	} else if volNum > 9 {
@@ -44,20 +70,10 @@ func (rec *uploadHandlerImpl) PostVolume(ctx echo.Context) error {
 		return errors.JsonError(ctx, http.StatusNotImplemented, msg)
 	}
 
-	body, err := io.ReadAll(ctx.Request().Body)
-	if err != nil {
-		msg := fmt.Sprintf("error reading request body: %v", err.Error())
-		log.Error().Err(err).Msg(msg)
-		return errors.JsonError(ctx, http.StatusBadRequest, msg)
-	}
-	doc := etree.NewDocument()
-	if err := doc.ReadFromBytes(body); err != nil {
-		return errors.JsonError(ctx, http.StatusBadRequest, fmt.Sprintf("error unmarshaling request body: %v", err.Error()))
-	}
 	if err := rec.volumeProcessor.Process(ctx.Request().Context(), doc); err != nil {
-		msg := fmt.Sprintf("error processing XML data for volume %d: %v", volNum, err.Error())
+		msg := fmt.Sprintf("error processing XML data for volume %d", volNum)
 		log.Error().Err(err).Msg(msg)
-		return errors.JsonError(ctx, http.StatusBadRequest, msg)
+		return errors.JsonError(ctx, http.StatusInternalServerError, msg)
 	}
 
 	return ctx.NoContent(http.StatusCreated)
