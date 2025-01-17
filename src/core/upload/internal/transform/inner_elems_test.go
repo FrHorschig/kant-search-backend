798d0930b1b441df03c15ff66c837f9649219771
Friedrich Horschig <friedrich.horschig@mailbox.org>
2025-01-17 15:39:49 +0100

    capitalize test names for consistency

--- src/core/upload/internal/transform/inner_elems_test.go
+++ src/core/upload/internal/transform/inner_elems_test.go
@@ -19,48 +19,48 @@ func TestAntiqua(t *testing.T) {
 		expectError bool
 	}{
 		{
-			name:     "pure text",
+			name:     "Pure text",
 			text:     "Some text",
 			expected: "Some text",
 		},
 		{
-			name:     "text with fett child element",
+			name:     "Text with fett child element",
 			text:     "Test text",
 			child:    createElement("fett", nil, "fettText", nil),
 			expected: "Test text <ks-fmt-bold>fettText</ks-fmt-bold>",
 		},
 		{
-			name:     "text with gesperrt child element",
+			name:     "Text with gesperrt child element",
 			text:     "Test text",
 			child:    createElement("gesperrt", nil, "gesperrtText", nil),
 			expected: "Test text <ks-fmt-tracked>gesperrtText</ks-fmt-tracked>",
 		},
 		{
-			name:     "text with name child element",
+			name:     "Text with name child element",
 			text:     "Test text",
 			child:    createElement("name", nil, "nameText", nil),
 			expected: "Test text nameText",
 		},
 		{
-			name:     "text with seite child element",
+			name:     "Text with seite child element",
 			text:     "Test text",
 			child:    createElement("seite", map[string]string{"nr": "384"}, "", nil),
 			expected: "Test text <ks-meta-page>384</ks-meta-page>",
 		},
 		{
-			name:     "text with zeile child element",
+			name:     "Text with zeile child element",
 			text:     "Test text",
 			child:    createElement("zeile", map[string]string{"nr": "328"}, "", nil),
 			expected: "Test text <ks-meta-line>328</ks-meta-line>",
 		},
 		{
-			name:     "text with trenn child element",
+			name:     "Text with trenn child element",
 			text:     "Test text",
 			child:    createElement("trenn", nil, "trennText", nil),
 			expected: "Test text",
 		},
 		{
-			name:     "text with leading and trailing spaces",
+			name:     "Text with leading and trailing spaces",
 			text:     "   Test text       ",
 			child:    nil,
 			expected: "Test text",
@@ -90,16 +90,16 @@ func TestBildBildverweis(t *testing.T) {
 		expected string
 	}{
 		{
-			name:     "default values due to missing attributes",
+			name:     "Default values due to missing attributes",
 			expected: `{image-extract src="MISSING_IMG_SRC" desc="MISSING_IMG_DESC"}`,
 		},
 		{
-			name:     "bild attributes are extracted",
+			name:     "Bild attributes are extracted",
 			attrs:    map[string]string{"src": "source string", "beschreibung": "description text"},
 			expected: `{image-extract src="source string" desc="description text"}`,
 		},
 		{
-			name:     "text is ignored",
+			name:     "Text is ignored",
 			attrs:    map[string]string{"src": "s", "beschreibung": "d"},
 			text:     "some text",
 			expected: `{image-extract src="s" desc="d"}`,
@@ -125,12 +125,12 @@ func TestEm1(t *testing.T) {
 		expected string
 	}{
 		{
-			name:     "text is extracted",
+			name:     "Text is extracted",
 			text:     "Something",
 			expected: "<ks-fmt-emph>Something</ks-fmt-emph>",
 		},
 		{
-			name:     "space is trimmed",
+			name:     "Space is trimmed",
 			text:     " Some text     ",
 			expected: "<ks-fmt-emph>Some text</ks-fmt-emph>",
 		},
@@ -154,96 +154,96 @@ func TestEm2(t *testing.T) {
 		expectError bool
 	}{
 		{
-			name:     "pure text",
+			name:     "Pure text",
 			text:     "Some emph2 text",
 			expected: "<ks-fmt-emph2>Some emph2 text</ks-fmt-emph2>",
 		},
 		{
-			name:     "text with bild child element",
+			name:     "Text with bild child element",
 			text:     "Test text",
 			child:    createElement("bild", map[string]string{"src": "source", "beschreibung": "description text"}, "", nil),
 			expected: `<ks-fmt-emph2>Test text {image-extract src="source" desc="description text"}</ks-fmt-emph2>`,
 		},
 		{
-			name:     "text with bildverweis child element",
+			name:     "Text with bildverweis child element",
 			text:     "Test text",
 			child:    createElement("bildverweis", map[string]string{"src": "source", "beschreibung": "description text"}, "", nil),
 			expected: `<ks-fmt-emph2>Test text {image-extract src="source" desc="description text"}</ks-fmt-emph2>`,
 		},
 		{
-			name:     "text with em1 child element",
+			name:     "Text with em1 child element",
 			text:     "Test text",
 			child:    createElement("em1", nil, "em1Text", nil),
 			expected: "<ks-fmt-emph2>Test text <ks-fmt-emph>em1Text</ks-fmt-emph></ks-fmt-emph2>",
 		},
 		{
-			name:     "text with em2 child element",
+			name:     "Text with em2 child element",
 			text:     "Test text",
 			child:    createElement("em2", nil, "em2Text", nil),
 			expected: "<ks-fmt-emph2>Test text <ks-fmt-emph2>em2Text</ks-fmt-emph2></ks-fmt-emph2>",
 		},
 		{
-			name:     "text with fett child element",
+			name:     "Text with fett child element",
 			text:     "Test text",
 			child:    createElement("fett", nil, "fettText", nil),
 			expected: "<ks-fmt-emph2>Test text <ks-fmt-bold>fettText</ks-fmt-bold></ks-fmt-emph2>",
 		},
 		{
-			name:     "text with formel child element",
+			name:     "Text with formel child element",
 			text:     "Test text",
 			child:    createElement("formel", nil, "formelText", nil),
 			expected: "<ks-fmt-emph2>Test text <ks-fmt-formula>formelText</ks-fmt-formula></ks-fmt-emph2>",
 		},
 		{
-			name:     "text with fr child element",
+			name:     "Text with fr child element",
 			text:     "Test text",
 			child:    createElement("fr", map[string]string{"seite": "1", "nr": "2"}, "", nil),
 			expected: "<ks-fmt-emph2>Test text <ks-fmt-fnref>1.2</ks-fmt-fnref></ks-fmt-emph2>",
 		},
 		{
-			name:     "text with fremdsprache child element",
+			name:     "Text with fremdsprache child element",
 			text:     "Test text",
 			child:    createElement("fremdsprache", nil, "fremdspracheText", nil),
 			expected: "<ks-fmt-emph2>Test text <ks-meta-lang>fremdspracheText</ks-meta-lang></ks-fmt-emph2>",
 		},
 		{
-			name:     "text with gesperrt child element",
+			name:     "Text with gesperrt child element",
 			text:     "Test text",
 			child:    createElement("gesperrt", nil, "gesperrtText", nil),
 			expected: "<ks-fmt-emph2>Test text <ks-fmt-tracked>gesperrtText</ks-fmt-tracked></ks-fmt-emph2>",
 		},
 		{
-			name:     "text with name child element",
+			name:     "Text with name child element",
 			text:     "Test text",
 			child:    createElement("name", nil, "nameText", nil),
 			expected: "<ks-fmt-emph2>Test text nameText</ks-fmt-emph2>",
 		},
 		{
-			name:     "text with romzahl child element",
+			name:     "Text with romzahl child element",
 			text:     "Test text",
 			child:    createElement("romzahl", nil, "2.", nil),
 			expected: "<ks-fmt-emph2>Test text II.</ks-fmt-emph2>",
 		},
 		{
-			name:     "text with seite child element",
+			name:     "Text with seite child element",
 			text:     "Test text",
 			child:    createElement("seite", map[string]string{"nr": "384"}, "", nil),
 			expected: "<ks-fmt-emph2>Test text <ks-meta-page>384</ks-meta-page></ks-fmt-emph2>",
 		},
 		{
-			name:     "text with trenn child element",
+			name:     "Text with trenn child element",
 			text:     "Test text",
 			child:    createElement("trenn", nil, "trennText", nil),
 			expected: "<ks-fmt-emph2>Test text</ks-fmt-emph2>",
 		},
 		{
-			name:     "text with zeile child element",
+			name:     "Text with zeile child element",
 			text:     "Test text",
 			child:    createElement("zeile", map[string]string{"nr": "328"}, "", nil),
 			expected: "<ks-fmt-emph2>Test text <ks-meta-line>328</ks-meta-line></ks-fmt-emph2>",
 		},
 		{
-			name:     "text with leading and trailing spaces",
+			name:     "Text with leading and trailing spaces",
 			text:     "   Test text       ",
 			expected: "<ks-fmt-emph2>Test text</ks-fmt-emph2>",
 		},
@@ -273,30 +273,30 @@ func TestFett(t *testing.T) {
 		expectError bool
 	}{
 		{
-			name:     "pure text",
+			name:     "Pure text",
 			text:     "Some bold text",
 			expected: "<ks-fmt-bold>Some bold text</ks-fmt-bold>",
 		},
 		{
-			name:     "text with seite child element",
+			name:     "Text with seite child element",
 			text:     "Test text",
 			child:    createElement("seite", map[string]string{"nr": "384"}, "", nil),
 			expected: "<ks-fmt-bold>Test text <ks-meta-page>384</ks-meta-page></ks-fmt-bold>",
 		},
 		{
-			name:     "text with zeile child element",
+			name:     "Text with zeile child element",
 			text:     "Test text",
 			child:    createElement("zeile", map[string]string{"nr": "328"}, "", nil),
 			expected: "<ks-fmt-bold>Test text <ks-meta-line>328</ks-meta-line></ks-fmt-bold>",
 		},
 		{
-			name:     "text with trenn child element",
+			name:     "Text with trenn child element",
 			text:     "Test text",
 			child:    createElement("trenn", nil, "trennText", nil),
 			expected: "<ks-fmt-bold>Test text</ks-fmt-bold>",
 		},
 		{
-			name:     "text with leading and trailing spaces",
+			name:     "Text with leading and trailing spaces",
 			text:     "   Test text       ",
 			child:    nil,
 			expected: "<ks-fmt-bold>Test text</ks-fmt-bold>",
@@ -326,27 +326,27 @@ func TestFr(t *testing.T) {
 		expected string
 	}{
 		{
-			name:     "page and number are extracted",
+			name:     "Page and number are extracted",
 			attrs:    map[string]string{"seite": "27", "nr": "254"},
 			expected: "<ks-fmt-fnref>27.254</ks-fmt-fnref>",
 		},
 		{
-			name:     "default values are used due to missing attributes",
+			name:     "Default values are used due to missing attributes",
 			expected: "<ks-fmt-fnref>MISSING_FNREF_PAGE.MISSING_FNREF_NUMBER</ks-fmt-fnref>",
 		},
 		{
-			name:     "text is ignored",
+			name:     "Text is ignored",
 			text:     "Some text",
 			attrs:    map[string]string{"seite": "223845", "nr": "5"},
 			expected: "<ks-fmt-fnref>223845.5</ks-fmt-fnref>",
 		},
 		{
-			name:     "attribute is non-numerical strings",
+			name:     "Attribute is non-numerical strings",
 			attrs:    map[string]string{"seite": "skdhsi", "nr": "sdk"},
 			expected: "<ks-fmt-fnref>skdhsi.sdk</ks-fmt-fnref>",
 		},
 		{
-			name:     "attributes with leading and trailing spaces",
+			name:     "Attributes with leading and trailing spaces",
 			attrs:    map[string]string{"seite": "  8  ", "nr": " 2     "},
 			expected: "<ks-fmt-fnref>8.2</ks-fmt-fnref>",
 		},
@@ -371,102 +371,102 @@ func TestFremdsprache(t *testing.T) {
 		expectError bool
 	}{
 		{
-			name:     "pure text",
+			name:     "Pure text",
 			text:     "Some foreing language text",
 			expected: "<ks-meta-lang>Some foreing language text</ks-meta-lang>",
 		},
 		{
-			name:     "text with language attributes",
+			name:     "Text with language attributes",
 			text:     "Some foreign language text",
 			attrs:    map[string]string{"sprache": "language", "zeichen": "some alphabet", "umschrift": "transcribed text"},
 			expected: `<ks-meta-lang lang="language" alphabet="some alphabet" transcript="transcribed text">Some foreign language text</ks-meta-lang>`,
 		},
 		{
-			name:     "text with bild child element",
+			name:     "Text with bild child element",
 			text:     "Test text",
 			child:    createElement("bild", map[string]string{"src": "source", "beschreibung": "description text"}, "", nil),
 			expected: `<ks-meta-lang>Test text {image-extract src="source" desc="description text"}</ks-meta-lang>`,
 		},
 		{
-			name:     "text with bildverweis child element",
+			name:     "Text with bildverweis child element",
 			text:     "Test text",
 			child:    createElement("bildverweis", map[string]string{"src": "source", "beschreibung": "description text"}, "", nil),
 			expected: `<ks-meta-lang>Test text {image-extract src="source" desc="description text"}</ks-meta-lang>`,
 		},
 		{
-			name:     "text with em1 child element",
+			name:     "Text with em1 child element",
 			text:     "Test text",
 			child:    createElement("em1", nil, "em1Text", nil),
 			expected: "<ks-meta-lang>Test text <ks-fmt-emph>em1Text</ks-fmt-emph></ks-meta-lang>",
 		},
 		{
-			name:     "text with em2 child element",
+			name:     "Text with em2 child element",
 			text:     "Test text",
 			child:    createElement("em2", nil, "em2Text", nil),
 			expected: "<ks-meta-lang>Test text <ks-fmt-emph2>em2Text</ks-fmt-emph2></ks-meta-lang>",
 		},
 		{
-			name:     "text with fett child element",
+			name:     "Text with fett child element",
 			text:     "Test text",
 			child:    createElement("fett", nil, "fettText", nil),
 			expected: "<ks-meta-lang>Test text <ks-fmt-bold>fettText</ks-fmt-bold></ks-meta-lang>",
 		},
 		{
-			name:     "text with formel child element",
+			name:     "Text with formel child element",
 			text:     "Test text",
 			child:    createElement("formel", nil, "formelText", nil),
 			expected: "<ks-meta-lang>Test text <ks-fmt-formula>formelText</ks-fmt-formula></ks-meta-lang>",
 		},
 		{
-			name:     "text with fr child element",
+			name:     "Text with fr child element",
 			text:     "Test text",
 			child:    createElement("fr", map[string]string{"seite": "1", "nr": "2"}, "", nil),
 			expected: "<ks-meta-lang>Test text <ks-fmt-fnref>1.2</ks-fmt-fnref></ks-meta-lang>",
 		},
 		{
-			name:     "text with fremdsprache child element",
+			name:     "Text with fremdsprache child element",
 			text:     "Test text",
 			child:    createElement("fremdsprache", nil, "fremdspracheText", nil),
 			expected: "<ks-meta-lang>Test text <ks-meta-lang>fremdspracheText</ks-meta-lang></ks-meta-lang>",
 		},
 		{
-			name:     "text with gesperrt child element",
+			name:     "Text with gesperrt child element",
 			text:     "Test text",
 			child:    createElement("gesperrt", nil, "gesperrtText", nil),
 			expected: "<ks-meta-lang>Test text <ks-fmt-tracked>gesperrtText</ks-fmt-tracked></ks-meta-lang>",
 		},
 		{
-			name:     "text with name child element",
+			name:     "Text with name child element",
 			text:     "Test text",
 			child:    createElement("name", nil, "nameText", nil),
 			expected: "<ks-meta-lang>Test text nameText</ks-meta-lang>",
 		},
 		{
-			name:     "text with romzahl child element",
+			name:     "Text with romzahl child element",
 			text:     "Test text",
 			child:    createElement("romzahl", nil, "2.", nil),
 			expected: "<ks-meta-lang>Test text II.</ks-meta-lang>",
 		},
 		{
-			name:     "text with seite child element",
+			name:     "Text with seite child element",
 			text:     "Test text",
 			child:    createElement("seite", map[string]string{"nr": "384"}, "", nil),
 			expected: "<ks-meta-lang>Test text <ks-meta-page>384</ks-meta-page></ks-meta-lang>",
 		},
 		{
-			name:     "text with trenn child element",
+			name:     "Text with trenn child element",
 			text:     "Test text",
 			child:    createElement("trenn", nil, "trennText", nil),
 			expected: "<ks-meta-lang>Test text</ks-meta-lang>",
 		},
 		{
-			name:     "text with zeile child element",
+			name:     "Text with zeile child element",
 			text:     "Test text",
 			child:    createElement("zeile", map[string]string{"nr": "328"}, "", nil),
 			expected: "<ks-meta-lang>Test text <ks-meta-line>328</ks-meta-line></ks-meta-lang>",
 		},
 		{
-			name:     "text with leading and trailing spaces",
+			name:     "Text with leading and trailing spaces",
 			text:     "   Test text       ",
 			child:    nil,
 			expected: "<ks-meta-lang>Test text</ks-meta-lang>",
@@ -500,42 +500,42 @@ func TestGesperrt(t *testing.T) {
 		expectError bool
 	}{
 		{
-			name:     "pure text",
+			name:     "Pure text",
 			text:     "Some tracked text",
 			expected: "<ks-fmt-tracked>Some tracked text</ks-fmt-tracked>",
 		},
 		{
-			name:     "text with fett child element",
+			name:     "Text with fett child element",
 			text:     "Test text",
 			child:    createElement("fett", nil, "fettText", nil),
 			expected: "<ks-fmt-tracked>Test text <ks-fmt-bold>fettText</ks-fmt-bold></ks-fmt-tracked>",
 		},
 		{
-			name:     "text with name child element",
+			name:     "Text with name child element",
 			text:     "Test text",
 			child:    createElement("name", nil, "nameText", nil),
 			expected: "<ks-fmt-tracked>Test text nameText</ks-fmt-tracked>",
 		},
 		{
-			name:     "text with seite child element",
+			name:     "Text with seite child element",
 			text:     "Test text",
 			child:    createElement("seite", map[string]string{"nr": "384"}, "", nil),
 			expected: "<ks-fmt-tracked>Test text <ks-meta-page>384</ks-meta-page></ks-fmt-tracked>",
 		},
 		{
-			name:     "text with trenn child element",
+			name:     "Text with trenn child element",
 			text:     "Test text",
 			child:    createElement("trenn", nil, "trennText", nil),
 			expected: "<ks-fmt-tracked>Test text</ks-fmt-tracked>",
 		},
 		{
-			name:     "text with zeile child element",
+			name:     "Text with zeile child element",
 			text:     "Test text",
 			child:    createElement("zeile", map[string]string{"nr": "328"}, "", nil),
 			expected: "<ks-fmt-tracked>Test text <ks-meta-line>328</ks-meta-line></ks-fmt-tracked>",
 		},
 		{
-			name:     "text with leading and trailing spaces",
+			name:     "Text with leading and trailing spaces",
 			text:     "   Test text       ",
 			child:    nil,
 			expected: "<ks-fmt-tracked>Test text</ks-fmt-tracked>",
@@ -566,30 +566,30 @@ func TestName(t *testing.T) {
 		expectError bool
 	}{
 		{
-			name:     "pure text",
+			name:     "Pure text",
 			text:     "Some name",
 			expected: "Some name",
 		},
 		{
-			name:     "text with seite child element",
+			name:     "Text with seite child element",
 			text:     "Test text",
 			child:    createElement("seite", map[string]string{"nr": "384"}, "", nil),
 			expected: "Test text <ks-meta-page>384</ks-meta-page>",
 		},
 		{
-			name:     "text with trenn child element",
+			name:     "Text with trenn child element",
 			text:     "Test text",
 			child:    createElement("trenn", nil, "trennText", nil),
 			expected: "Test text",
 		},
 		{
-			name:     "text with zeile child element",
+			name:     "Text with zeile child element",
 			text:     "Test text",
 			child:    createElement("zeile", map[string]string{"nr": "328"}, "", nil),
 			expected: "Test text <ks-meta-line>328</ks-meta-line>",
 		},
 		{
-			name:     "text with leading and trailing spaces",
+			name:     "Text with leading and trailing spaces",
 			text:     "   Test text       ",
 			child:    nil,
 			expected: "Test text",
@@ -619,17 +619,17 @@ func TestRomzahl(t *testing.T) {
 		expectError bool
 	}{
 		{
-			name:     "number is converted with dot",
+			name:     "Number is converted with dot",
 			text:     "14.",
 			expected: "XIV.",
 		},
 		{
-			name:     "number is converted without dot",
+			name:     "Number is converted without dot",
 			text:     "116",
 			expected: "CXVI",
 		},
 		{
-			name:     "non-number is ignored",
+			name:     "Non-number is ignored",
 			text:     "anfk.",
 			expected: "",
 		},
@@ -654,28 +654,28 @@ func TestZeile(t *testing.T) {
 		expectError bool
 	}{
 		{
-			name:     "number is extracted",
+			name:     "Number is extracted",
 			attrs:    map[string]string{"nr": "254"},
 			expected: "<ks-meta-line>254</ks-meta-line>",
 		},
 		{
-			name:     "text is ignored",
+			name:     "Text is ignored",
 			text:     "Some text",
 			attrs:    map[string]string{"nr": "847"},
 			expected: "<ks-meta-line>847</ks-meta-line>",
 		},
 		{
-			name:     "nr attribute with leading and trailing spaces",
+			name:     "Nr attribute with leading and trailing spaces",
 			attrs:    map[string]string{"nr": " 2     "},
 			expected: "<ks-meta-line>2</ks-meta-line>",
 		},
 		{
-			name:        "error due to missing number",
+			name:        "Error due to missing number",
 			expected:    "",
 			expectError: true,
 		},
 		{
-			name:        "error due to non-numerical number",
+			name:        "Error due to non-numerical number",
 			attrs:       map[string]string{"nr": "abf382"},
 			expected:    "",
 			expectError: true,
