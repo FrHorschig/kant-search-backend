e07a859f21d3619439375848c8f409f16646ad55
Friedrich Horschig <friedrich.horschig@mailbox.org>
2025-01-24 12:03:35 +0100

    adjust database model

--- src/dataaccess/model/model.go
+++ src/dataaccess/model/model.go
@@ -1,9 +1,6 @@
 package model
 
-import "github.com/frhorschig/kant-search-backend/common/model"
-
 type Volume struct {
-	Id      int32
 	Section int32
 	Title   string
 	Works   []Work
@@ -12,29 +9,58 @@ type Volume struct {
 type Work struct {
 	Id           int32
 	Code         string
+	Title        string
 	Abbreviation *string
 	Year         *string
+	TextData     Section
+	Footnotes    []Footnote
 	Volume       int32
 }
 
-type Heading struct {
-	Id     int32
-	Level  model.Level
-	Text   string
-	WorkId int32
+type Section struct {
+	Id         int32
+	Heading    Heading
+	Paragraphs []Paragraph
+	Sections   []Section
+	Parent     *Section
 }
 
+type Heading struct {
+	Id        int32
+	Level     Level
+	TocTitle  string
+	TextTitle string
+	WorkId    int32
+}
+
+type Level int32
+
+const (
+	HWork Level = iota + 1
+	H1
+	H2
+	H3
+	H4
+	H5
+	H6
+	H7
+)
+
 type Paragraph struct {
-	Id     int32
-	Text   string
-	Pages  []int32
-	WorkId int32
+	Id           int32
+	Text         string
+	Pages        []int32
+	FnReferences []int32
+	Sentences    []Sentence
+	WorkId       int32
 }
 
 type Sentence struct {
 	Id          int32
 	Text        string
+	Pages       []int32
 	ParagraphId int32
+	WorkId      int32
 }
 
 type Footnote struct {
