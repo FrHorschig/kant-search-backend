ba6874e4e2a90ac60500afa14706720656a7413d
Friedrich Horschig <friedrich.horschig@mailbox.org>
2025-03-27 20:30:46 +0100

    adjust page extraction and its tests

--- src/core/upload/internal/extract/extract.go
+++ src/core/upload/internal/extract/extract.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"regexp"
 	"strconv"
+	"strings"
 
 	"github.com/frhorschig/kant-search-backend/common/errors"
 	"github.com/frhorschig/kant-search-backend/core/upload/internal/model"
@@ -30,6 +31,11 @@ func ExtractPages(text string) ([]int32, errors.ErrorNew) {
 			return nil, errors.NewError(nil, fmt.Errorf("error converting string '%s' to number: %v", nStr, err.Error()))
 		}
 		result = append(result, int32(n))
+	}
+
+	parts := strings.Split(text, strings.Split(model.PageFmt, "%")[0])
+	if parts[0] != "" && len(result) > 0 {
+		result = append([]int32{result[0] - 1}, result...)
 	}
 	return result, errors.NilError()
 }
