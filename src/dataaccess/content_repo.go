d15bbf8b419a3a3f64511c7c44b5c9b4195a3955
Friedrich Horschig <friedrich.horschig@mailbox.org>
2025-04-11 11:42:42 +0200

    add repository methods

--- src/dataaccess/content_repo.go
+++ src/dataaccess/content_repo.go
@@ -1,10 +1,23 @@
 package dataaccess
 
-import "github.com/elastic/go-elasticsearch/v8"
+import (
+	"context"
+
+	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/frhorschig/kant-search-backend/dataaccess/internal/esmodel"
+)
 
 //go:generate mockgen -source=$GOFILE -destination=mocks/content_repo_mock.go -package=mocks
 
 type ContentRepo interface {
+	InsertHeading(ctx context.Context, data esmodel.Heading) error
+	DeleteHeading(ctx context.Context, id int32) error
+	InsertParagraph(ctx context.Context, data esmodel.Paragraph) error
+	DeleteParagraph(ctx context.Context, id int32) error
+	InsertFootnote(ctx context.Context, data esmodel.Footnote) error
+	DeleteFootnote(ctx context.Context, id int32) error
+	InsertSummary(ctx context.Context, data esmodel.Summary) error
+	DeleteSummary(ctx context.Context, id int32) error
 }
 
 type contentRepoImpl struct {
@@ -15,4 +28,44 @@ func NewContentRepo(dbClient *elasticsearch.TypedClient) ContentRepo {
 	return &contentRepoImpl{
 		dbClient: dbClient,
 	}
+}
+
+func (rec *contentRepoImpl) InsertHeading(ctx context.Context, data esmodel.Heading) error {
+	// TODO implement me
+	return nil
+}
+
+func (rec *contentRepoImpl) DeleteHeading(ctx context.Context, id int32) error {
+	// TODO implement me
+	return nil
+}
+
+func (rec *contentRepoImpl) InsertParagraph(ctx context.Context, data esmodel.Paragraph) error {
+	// TODO implement me
+	return nil
+}
+
+func (rec *contentRepoImpl) DeleteParagraph(ctx context.Context, id int32) error {
+	// TODO implement me
+	return nil
+}
+
+func (rec *contentRepoImpl) InsertFootnote(ctx context.Context, data esmodel.Footnote) error {
+	// TODO implement me
+	return nil
+}
+
+func (rec *contentRepoImpl) DeleteFootnote(ctx context.Context, id int32) error {
+	// TODO implement me
+	return nil
+}
+
+func (rec *contentRepoImpl) InsertSummary(ctx context.Context, data esmodel.Summary) error {
+	// TODO implement me
+	return nil
+}
+
+func (rec *contentRepoImpl) DeleteSummary(ctx context.Context, id int32) error {
+	// TODO implement me
+	return nil
 }
--- src/dataaccess/volume_repo.go
+++ src/dataaccess/volume_repo.go
@@ -3,10 +3,15 @@ package dataaccess
 //go:generate mockgen -source=$GOFILE -destination=mocks/volume_repo_mock.go -package=mocks
 
 import (
+	"context"
+
 	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/frhorschig/kant-search-backend/dataaccess/internal/esmodel"
 )
 
 type VolumeRepo interface {
+	Insert(ctx context.Context, volNum int32, data esmodel.Volume) error
+	Delete(ctx context.Context, volNum int32) error
 }
 
 type volumeRepoImpl struct {
@@ -17,4 +22,14 @@ func NewVolumeRepo(dbClient *elasticsearch.TypedClient) VolumeRepo {
 	return &volumeRepoImpl{
 		dbClient: dbClient,
 	}
+}
+
+func (rec *volumeRepoImpl) Insert(ctx context.Context, volNum int32, data esmodel.Volume) error {
+	// TODO implement me
+	return nil
+}
+
+func (rec *volumeRepoImpl) Delete(ctx context.Context, volNum int32) error {
+	// TODO implement me
+	return nil
 }
