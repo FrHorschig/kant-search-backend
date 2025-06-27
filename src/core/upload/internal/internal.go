8aec12b6d396544255849371ca11d0c9d834561f
Friedrich Horschig <frhorschig-coding@mailbox.org>
2025-06-27 12:34:32 +0200

    add new pkg for postprocessing after xml mapping

--- src/core/upload/internal/internal.go
+++ src/core/upload/internal/internal.go
@@ -6,6 +6,7 @@ import (
 	"github.com/beevik/etree"
 	"github.com/frhorschig/kant-search-backend/common/errs"
 	"github.com/frhorschig/kant-search-backend/core/upload/internal/metadata"
+	"github.com/frhorschig/kant-search-backend/core/upload/internal/metadatamapping/metadatamapping"
 	"github.com/frhorschig/kant-search-backend/core/upload/internal/model"
 	"github.com/frhorschig/kant-search-backend/core/upload/internal/modelmap"
 	"github.com/frhorschig/kant-search-backend/core/upload/internal/treemap"
@@ -14,6 +15,7 @@ import (
 	"github.com/frhorschig/kant-search-backend/core/upload/internal/xmlmapping/referencemapping"
 	"github.com/frhorschig/kant-search-backend/core/upload/internal/xmlmapping/textmapping"
 	"github.com/frhorschig/kant-search-backend/core/upload/internal/xmlmapping/treemapping"
+	dbmodel "github.com/frhorschig/kant-search-backend/dataaccess/model"
 )
 
 type XmlMapper interface {
@@ -67,19 +69,23 @@ func (rec *xmlMapperImpl) MapWorks(volNr int32, xml string) ([]model.Work, errs.
 
 // =============================================================================
 
-type XmlMapperNew interface {
-	MapWorks(volNr int32, xml string) ([]model.Work, errs.UploadError)
+type XmlMapperNew interface { // TODO find appropriate name
+	MapXml(volNr int32, xml string) (dbmodel.Volume, []dbmodel.Content, errs.UploadError)
 }
 
 type xmlMapperNewImpl struct {
+	metadata metadata.Metadata
 }
 
-func NewXmlMapperNew() XmlMapperNew {
-	impl := xmlMapperNewImpl{}
+func NewXmlMapperNew(metadata metadata.Metadata) XmlMapperNew {
+	impl := xmlMapperNewImpl{
+		metadata: metadata,
+	}
 	return &impl
 }
 
-func (rec *xmlMapperNewImpl) MapWorks(volNr int32, xml string) ([]model.Work, errs.UploadError) {
+func (rec *xmlMapperNewImpl) MapXml(volNr int32, xml string) (dbmodel.Volume, []dbmodel.Content, errs.UploadError) {
+	// map xml to model
 	works, fns, summs, err := treemapping.MapTree(xml)
 	if err.HasError {
 		return nil, err
@@ -96,9 +102,17 @@ func (rec *xmlMapperNewImpl) MapWorks(volNr int32, xml string) ([]model.Work, er
 	if err.HasError {
 		return nil, err
 	}
+
+	// add non-xml metadata to model
 	err = ordering.Order(works)
 	if err.HasError {
 		return nil, err
 	}
-	return works, errs.Nil()
+	volTitle, err := metadatamapping.MapMetadata(volNr, works, rec.metadata)
+	if err.HasError {
+		return nil, err
+	}
+
+	// map to db model
+	panic("implement me")
 }
